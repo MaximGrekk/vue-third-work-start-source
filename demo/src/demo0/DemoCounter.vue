2a9fb8607b231b2a85b872dacf085d9a7067ad9e
Maxim Sergeev <MaximGrekk@gmail.com>
2023-07-13 00:13:09 +0300

    Implements mocs into the HomeView template, modified the App.vue file

--- demo/src/demo0/DemoCounter.vue
+++ demo/src/demo0/DemoCounter.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
-    <button> - </button>
-    <span>Current value: 0</span>
-    <div> + </div>
+    <button @click="decrement">-</button>
+
+    <span> Current value: {{ count }} </span>
+
+    <button @click="increment">+</button>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+/* Метод уменьшения значения */
+const decrement = () => {
+  count.value = count.value - 1;
+};
+
+/* Метод увеличения значения */
+const increment = () => {
+  count.value = count.value + 1;
+};
+</script>
