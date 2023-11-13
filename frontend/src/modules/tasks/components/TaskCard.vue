c2179d565f727d12c3de72c8faf2902d1e5e56a7
Maxim Sergeev <maximsergeev@MacBook-Pro-Maxim.local>
2023-11-13 12:53:10 +0300

    Added example tests for some components

--- frontend/src/modules/tasks/components/TaskCard.vue
+++ frontend/src/modules/tasks/components/TaskCard.vue
@@ -29,7 +29,11 @@
             :class="`task__status--${task.timeStatus}`"
           />
         </div>
-        <h5 class="task__title" :class="{ 'task__title--first': !task.user }">
+        <h5
+          class="task__title"
+          data-test="task-title"
+          :class="{ 'task__title--first': !task.user }"
+        >
           {{ task.title }}
         </h5>
         <!--        Тэги задачи вынесены в отдельный компонент-->
